f6ba45dc605d9c5083e0c859a4a6cfed0e7e67e8
Sander Voss <sander@rolisa.nl>
2017-12-16 21:05:13 +0000

    Untracked unnecessary files

--- Old/extrude_test.m
+++
@@ -1,7 +0,0 @@
-      q = linspace(0,2*pi,33);
-      base = 0.2*[cos(q); sin(q)];   % Base curve is a circle (radius = 1)
-      q = linspace(0,2*pi,101);
-      traj = 5*[cos(q); sin(q); 0*q]; %Trajectory is a circle (radius = 5)
-
-      [X,Y,Z] = extrude(base,test);
-      figure; surf(X,Y,Z); axis equal;
